931fba6e0596ddb60fbbb628fe6126d3141cc5c1
Marcus Lau <niteykid1@gmail.com>
2019-10-10 20:51:30 -0700

    fixed incorrect naming

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
     let city = e.target.elements.city.value;
     let country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const response = await api_call.json();
 
     console.log(response);
--- src/Title.js
+++ src/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = (props) => {
     return (
         <div>
-            <h1 clasName="title-container__title">Weather Scanner</h1>
+            <h1 className="title-container__title">Weather Scanner</h1>
             <p className="title-container__subtitle">Hoes never get cold...</p>
         </div>
     )
